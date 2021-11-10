ba45b3209031fd216f0bc9840674941dbb42a981
mamonarchu <radanladar@protonmail.com>
2021-11-10 23:11:05 +0100

    commit v0.1

---
+++ README.md
@@ -0,0 +1,10 @@
+# pro-manager
+A simple set of scripts to manage a small company invoices. 
+
+## FEATURES
+This scripts allows to input infos following the format "Customer's name, Sold Item, Price, VAT, Payement Due, Status(paid/not paid)"
+- 1) Input
+- 2) Read full list
+- 3) Search...
+- 4) Config
+All the data is saved into a CSV file.
---
+++ invoices-manager.sh
@@ -0,0 +1,190 @@
+#/bin/bash
+
+# ---------INFO---------
+# Invoices management script
+# github.com/RadanLadar/pro-manager
+
+# -----DEPENDENCIES-----
+# -bc ; floating-numbers computing
+# -figlet ; header txt
+
+# -------- TODO---------
+# - Check for improvements
+
+# GITHUB CONNECT CHECK
+
+# Setup path
+path=`dirname $(realpath $0)`
+
+# User prompt init
+choice=""
+
+# Clear the terminal when the program starts
+clear
+
+# Loops to avoid unwanted exits
+while [ choice != "q" ]
+do # Everything below must be reload for update
+
+# Import VAT value
+vatVal=$(cat vat.txt)
+
+# Header
+figlet invoice
+figlet manager
+echo "[v0.1]"
+echo -------------------------------------------------------------
+
+# Display entry choice
+
+echo What do you want to do ?
+echo -------------------------------------------------------------
+echo ""
+column -t -s '\!' $path/menu.txt
+echo ""
+
+# Get the choice 
+echo -------------------------------------------------------------
+read -p 'Your choice: ' choice
+echo -------------------------------------------------------------
+	# Processing the choice
+	case $choice in
+		1)
+			# Get the index from a file 
+			index=$(cat $path/index.txt)
+			
+			# Getting user prompt
+			echo 'ADDING A NEW INVOICE'
+			echo ""
+			read -p 'Company name: ' customername
+			read -p 'Type of mission: ' missiontype
+			read -p 'Contract price: ' price
+			read -p 'VAT appliable? (y/n): ' isVat
+				
+				# Include VAT if needed
+				case $isVat in
+					y)	
+						# Compute variables		
+						vatAdded=`echo "$vatVal * $price" | bc`
+						vat=`echo "$vatAdded - $price" | bc`
+						echo ""
+						# Display the computed infos
+						echo The VAT value is $vat
+						echo Full price is $vatAdded
+						;;
+					n)
+						vatAdded=$price
+						echo ""
+						echo Full price is $vatAdded
+						;;
+					*)
+						echo ""
+			 			echo Wrong input
+						;;
+				esac
+
+			# Increment the index value
+			((index=index+1))
+			echo $index > $path/index.txt
+			# Add the infos at the bottom of the db file
+			
+			echo $index\! $customername\! $missiontype\! $vatAdded\! $vat >> $path/invoicesdb.csv
+			echo Invoice added successfully!
+			;;
+		2)
+			echo ""
+			echo .......................FULL LIST.............................
+			column -t -s '\!' $path/header.csv $path/invoicesdb.csv
+			;;
+		3)
+			read -p 'Search... ' srcvar
+			while IFS= read -r line; do
+				if [[ "$line" == *"$srcvar"* ]]; then
+					printf '%s\n' "$line" >> $path/srcresult.txt
+				fi
+			done < $path/invoicesdb.csv 
+			
+			# Display results
+			column -t -s '\!' $path/header.csv $path/srcresult.txt	
+			;;
+		v)
+			echo ..........................CONFIG.............................
+			echo 1\) VAT value
+			echo '---> '$vatVal
+			echo ""
+			echo 2\) Params
+		        echo '---> ' 
+			column -t -s '\!' $path/header.csv
+			echo ""
+			echo b\) BACK
+			echo ""
+			echo _______________
+			echo r\) RESET ALL	
+			echo ""
+			read -p 'Access config num ' conf
+				case $conf in
+					1) 
+						read -p 'Type your local VAT value (float) ' vatConf
+						echo $vatConf > $path/vat.txt
+						;;
+					2)
+						echo This config is not available yet
+						;;
+					b)
+						echo "Back to main menu"
+						;;
+					r)
+						read -p 'Do you really want to erase all data? (y/n) ' reset
+						case $reset in 
+							y)
+								echo "0" > $path/index.txt
+								echo "" > $path/invoicesdb.csv
+								echo All data erased!
+								;;
+							n)
+echo Aborted!
+;;
+*)
+echo -n 'Wrong input'
+;;
+esac
+						;;
+					*)
+						echo Wrong input
+						;;
+				esac
+			;;
+		q)
+			clear
+			exit
+			;;
+
+		r)
+			read -p 'Do you really want to erase data? (y/n) ' reset
+				case $reset in
+					y)
+						echo "0" > $path/index.txt
+						echo "" > $path/invoicesdb.csv
+						echo All data erased!
+						;;
+					n)
+						echo Aborted!
+						;;
+					*)
+						echo -n 'Wrong input'
+						;;
+				esac
+			;;
+		*) 
+			echo -n 'Wrong choice' 
+			;;
+	esac
+
+	echo ""
+	read -p '(Press any key to continue)'
+
+	# reset research
+	echo "" > $path/srcresult.txt
+	clear
+
+done
