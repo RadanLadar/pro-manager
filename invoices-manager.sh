fd4baac2f14550ca7d7c76797d89171a448dc18f
mamonarchu <radanladar@protonmail.com>
2021-11-13 21:44:16 +0100

    code and display improvement

--- invoices-manager.sh
+++ invoices-manager.sh
@@ -48,150 +48,105 @@ $path/alert.sh
 read -p 'Your choice: ' choice
 echo ---------------------------------------------------------
 echo ""
-	# Processing the choice
-	case ${choice,,} in
-		1)
-			# Get the index from a file 
-			index=$(cat $path/index.txt)
-			
-			# Getting user prompt
-			echo 'ADDING A NEW INVOICE'
-			echo ""
-			read -p 'Company name: ' customername
-			read -p 'Type of mission: ' missiontype
-			read -p 'Contract price: ' price
-			read -p 'VAT appliable? (y/n): ' isVat
-				
-				# Include VAT if needed
-				case ${isVat,,} in
-					y)	
-						# Compute variables		
-						vatAdded=`echo "$vatVal * $price" | bc`
-						vat=`echo "$vatAdded - $price" | bc`
-						echo ""
-						# Display the computed infos
-						echo The VAT value is $vat
-						echo Full price is $vatAdded
-						;;
-					n)
-						vatAdded=$price
-						vat=""
-						echo ""
-						echo Full price is $vatAdded
-						;;
-					*)
-						echo ""
-			 			echo Wrong input
-						;;
-				esac
 
-			# Increment the index value
-			((index=index+1))
-			echo $index > $path/index.txt
-			# Add the infos at the bottom of the db file
-			numinv=$($path/numgen.sh)
-			echo The reference of the invoice is $numinv
-			echo ""
-			echo $index\!${customername^^}\!${missiontype^^}\!$price\!$vatAdded\!$vat\!$numinv >> $path/invoicesdb.csv
-			echo Invoice added successfully!
-			;;
+# Processing the choice
+case ${choice,,} in
+	1)
+		$path/newinvoice.sh
+		;;
 		# DISPLAY THE FULL LIST
-		2)
-			echo ""
-			echo .......................FULL LIST.............................
-			column -t -s '\!' $path/header.csv $path/invoicesdb.csv
-			;;
-		# SEARCH FEATURE
-		3)
-			read -p 'Search... ' srcvar
-			while IFS= read -r line; do
-				if [[ "$line" == *"${srcvar^^}"* ]]; then
-					printf '%s\n' "$line" >> $path/srcresult.txt
-				fi
-			done < $path/invoicesdb.csv 
-			
-			# Display results
-			column -t -s '\!' $path/header.csv $path/srcresult.txt	
-			;;
-		4)
-			$path/line-edit.sh
-			;;
-		v)
-			echo ..........................CONFIG.............................
-			echo 1\) VAT value
-			echo '---> '$vatVal
-			echo ""
-			echo 2\) Params
-		        echo '---> ' 
-			column -t -s '\!' $path/header.csv
-			echo ""
-			echo 3\) Deadline
-			echo '---> '$dl
-			echo ""
-			echo b\) BACK
-			echo ""
-			echo _______________
-			echo r\) RESET ALL	
-			echo ""
-			read -p 'Access config num ' conf
-				case ${conf,,} in
-					1) 
-						read -p 'Type your local VAT value (float) ' vatConf
-						echo $vatConf > $path/vat.txt
-						;;
-					2)
-						echo This config is not available yet
-						;;
-					3)
-						read -p 'Type the new digits for the deadline date ' dlConf
-						echo $dlConf > $path/dl.txt
-						;;
-					b)
-						echo "Back to main menu"
-						;;
-					r)
-						read -p 'Do you really want to erase all data? (y/n) ' reset
-						case ${reset,,} in 
-							y)
-								echo "0" > $path/index.txt
-								echo "" > $path/invoicesdb.csv
-								echo "000" > $path/q1.txt
-								echo "000" > $path/q2.txt
-								echo "000" > $path/q3.txt
-								echo "000" > $path/q4.txt
-								echo "" > $path/dl.txt
-								echo All data erased!
-								;;
-							n)
-								echo Aborted!
-								;;
-							*)
-								echo -n 'Wrong input'
-								;;
-						esac
-						;;
-					*)
-						echo Wrong input
-						;;
-				esac
-			;;
-		q)
-			clear
-			exit
-			;;
+	2)
+		echo ""
+		echo .......................FULL LIST.............................
+		column -t -s '\!' $path/header.csv $path/invoicesdb.csv
+		;;
+	# SEARCH FEATURE
+	3)
+		read -p 'Search... ' srcvar
+		while IFS= read -r line; do
+			if [[ "$line" == *"${srcvar^^}"* ]]; then
+				printf '%s\n' "$line" >> $path/srcresult.txt
+			fi
+		done < $path/invoicesdb.csv 
+		
+		# Display results
+		column -t -s '\!' $path/header.csv $path/srcresult.txt	
+		;;
+	4)
+		$path/line-edit.sh
+		;;
+	v)
+		echo ..........................CONFIG.............................
+		echo 1\) VAT value
+		echo '---> '$vatVal
+		echo ""
+		echo 2\) Params
+	        echo '---> ' 
+		column -t -s '\!' $path/header.csv
+		echo ""
+		echo 3\) Deadline
+		echo '---> '$dl
+		echo ""
+		echo b\) BACK
+		echo ""
+		echo _______________
+		echo r\) RESET ALL	
+		echo ""
+		read -p 'Access config num ' conf
+			case ${conf,,} in
+				1) 
+					read -p 'Type your local VAT value (float) ' vatConf
+					echo $vatConf > $path/vat.txt
+					;;
+				2)
+					echo This config is not available yet
+					;;
+				3)
+					read -p 'Type the new digits for the deadline date ' dlConf
+					echo $dlConf > $path/dl.txt
+					;;
+				b)
+					echo "Back to main menu"
+					;;
+				r)
+					read -p 'Do you really want to erase all data? (y/n) ' reset
+					case ${reset,,} in 
+						y)
+							echo "0" > $path/index.txt
+							echo "" > $path/invoicesdb.csv
+							echo "000" > $path/q1.txt
+							echo "000" > $path/q2.txt
+							echo "000" > $path/q3.txt
+							echo "000" > $path/q4.txt
+							echo "" > $path/dl.txt
+							echo All data erased!
+							;;
+						n)
+							echo Aborted!
+							;;
+						*)
+							echo -n 'Wrong input'
+							;;
+					esac
+					;;
+				*)
+					echo Wrong input
+					;;
+			esac
+		;;
+	q)
+		clear
+		exit
+		;;
+	*) 
+		echo -n 'Wrong choice' 
+		;;
+esac
+echo ""
+read -p '(Press any key to continue)'
 
-
-		*) 
-
-			echo -n 'Wrong choice' 
-			;;
-	esac
-
-	echo ""
-	read -p '(Press any key to continue)'
-
-	# reset research
-	echo "" > $path/srcresult.txt
-	clear
+# reset research
+echo "" > $path/srcresult.txt
+clear
 
 done
---
+++ newinvoice.sh
@@ -0,0 +1,62 @@
+#/bin/bash
+
+#############################
+#   add invoice component   #
+############################
+
+path=`dirname $(realpath $0)`
+
+index=$(cat $path/index.txt)
+vatVal=$(cat $path/vat.txt)
+# Getting user prompt
+echo    ╔═══════════════════════════════════════════════════════╗
+echo   '║                  ADDING A NEW INVOICE                 ║'
+echo    ╚═══════════════════════════════════════════════════════╝
+echo    ┌──────────────────┬────────────────────────────────────┐
+read -p '│ Company name:    │ ' customername
+echo    ├──────────────────┼────────────────────────────────────┤
+read -p '│ Type of mission: │ ' missiontype
+echo    ├──────────────────┼────────────────────────────────────┤
+read -p '│ Contract price:  │ ' price
+echo    ├──────────────────┼────────────────────────────────────┤
+read -p '│ VAT? (y/n):      │ ' isVat
+echo    └──────────────────┴────────────────────────────────────┘
+		
+# Include VAT if needed
+case ${isVat,,} in
+	y)	
+	# Compute variables		
+		vatAdded=`echo "$vatVal * $price" | bc`
+		vat=`echo "$vatAdded - $price" | bc`
+		echo ""
+	# Display the computed infos
+
+		echo '╓──────────────────┬────────────────────────────────────┐'
+		echo '║ The VAT value is │' $vat  
+		echo '║──────────────────┼────────────────────────────────────┤'
+		echo '║ Full price is    │' $vatAdded 
+		echo '║──────────────────┼────────────────────────────────────┤'
+		;;
+	n)
+		vatAdded=$price
+		vat=""
+		echo ""
+		echo Full price is $vatAdded
+		;;
+	*)
+		echo ""
+		echo Wrong input
+		;;
+	esac
+
+# Increment the index value
+((index=index+1))
+echo $index > $path/index.txt
+
+# Add the infos at the bottom of the db file
+numinv=$($path/numgen.sh)
+echo '║ Invoice ref.     │' $numinv
+echo '║──────────────────┴────────────────────────────────────┤'
+echo $index\!${customername^^}\!${missiontype^^}\!$price\!$vatAdded\!$vat\!$numinv >> $path/invoicesdb.csv
+echo '║ Invoice added successfully!                           │'
+echo '╙───────────────────────────────────────────────────────┘'
